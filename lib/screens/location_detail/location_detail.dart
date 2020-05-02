baad8cd77d71cb1b3f1780ae741a5197362c6d71
henrik392 <henrik.halvorsen.kvamme@gmail.com>
2020-05-02 22:26:38 +0200

    Giddkje mer med amd

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
 import 'image_banner.dart';
 import 'text_section.dart';
+import '../../models/location.dart';
 
 class LocationDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final locations = Location.fetchAll();
+    final location = locations.first;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('test'),
+        title: Text(location.name),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          ImageBanner("assets/images/kiyomizu-dera.jpg"),
+          ImageBanner(location.imagePath),
           TextSection("summary", "something1"),
           TextSection("summary",
           "gnringrngirnejgnerigeringirngirgiri ioei eio feiof eii nefne fenfio e fnef enf enfenfnfne neinfionfien neifneonfion ioenfennfioenf ioeniooinio io nefe e e e eeefefe efegrjigrejgre nejen ejnf efnenf en ienfin ifn ie eoi n"),
